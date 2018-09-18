pop-os/ubuntu-drivers-common

commit 6770360ed3324358d9069c7468a3af80219157ef

Check also /lib/modprobe.d when looking for blacklisted modules

share/hybrid/gpu-manager.c
@@ -381,6 +381,15 @@ static bool is_module_blacklisted(const char* module) {
                  module, modprobe_d_path);
 
         match = get_output(command, NULL, NULL);
+
+        if (!match) {
+            free(match);
+            snprintf(command, sizeof(command),
+                 "grep -G \"^blacklist.*%s[[:space:]]*$\" %s/*.conf",
+                 module, "/lib/modprobe.d");
+
+            match = get_output(command, NULL, NULL);
+        }
     }
 
     if (!match)

commit b040753b81219a46901f01d9ead5d8a4feea5131

Import patches-unapplied version 1:0.5.4 to ubuntu/cosmic-proposed
Imported using git-ubuntu import.

Changelog parent: f82564b12dd374351fa83783c47a3ddbb679577d

New changelog entries:
  * debian/source/options:
      - Ignore "__pycache__" dir.
  * debian/rules:
    - Make sure to remove all the "__pycache__" directories.
  * gpu-manager.c:
      - Fix potential issue introduced in commit
        6770360ed3324358d9069c7468a3af80219157ef.

debian/rules
@@ -47,4 +47,5 @@ override_dh_clean:
 	rm -rf build
 	rm -rf *.egg-info
 	rm -f debian/ubuntu-drivers-common.maintscript
+	find $(CURDIR) -name "__pycache__" -exec rm -rf "{}" \;
 	dh_clean

debian/source/options
@@ -1,0 +1,3 @@
+extend-diff-ignore = "*__pycache__*"
+tar-ignore = "*__pycache__*"
+

share/hybrid/gpu-manager.c
@@ -382,7 +382,6 @@ static bool is_module_blacklisted(const char* module) {
         match = get_output(command, NULL, NULL);
 
         if (!match) {
-            free(match);
             snprintf(command, sizeof(command),
                  "grep -G \"^blacklist.*%s[[:space:]]*$\" %s/*.conf",
                  module, "/lib/modprobe.d");
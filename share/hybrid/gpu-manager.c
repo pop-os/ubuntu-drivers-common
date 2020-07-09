a31a814f41ae00854ef02afec89671c3579f259b
Tim Crawford <tcrawford@system76.com>
2020-07-09 16:26:34 -0600

    gpu-manager: HACK: Give GPU enough time to initialize
    With 450.57 drivers, GPU screens must be disabled so they are not used
    for rendering displays. Because the device is not used, enabling power
    management causes it to be suspended before it finishes initializing.
    Add a sleep to give the drivers enough time to initialize the device.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- share/hybrid/gpu-manager.c
+++ share/hybrid/gpu-manager.c
@@ -2112,6 +2112,10 @@ unload_again:
         }
 #endif
 
+        // XXX: HACK: Give the GPU time to initialize
+        fprintf(log_handle, "Giving GPU time to initialize...\n");
+        sleep(1);
+
         /* Set power control to "auto" to save power */
         enable_power_management(device);
     }

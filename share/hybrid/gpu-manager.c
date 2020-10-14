12af1fd26b1155eb8ca5645587e4366ec0ef0114
Tim Crawford <tcrawford@system76.com>
2020-10-14 14:39:30 -0600

    gpu-manager: Don't unload NVIDIA drivers
    In Pop!_OS, the state of the modules is controlled by blacklisting them
    using system76-power. Stop forcibly unloading them so they are available
    without user intervention on system start. This allows using the dGPU as
    a compute node when the NVIDIA mode is set to 'off'.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- share/hybrid/gpu-manager.c
+++ share/hybrid/gpu-manager.c
@@ -1958,8 +1958,8 @@ static bool enable_prime(const char *prime_settings,
                         const struct device *device,
                         struct device **devices,
                         int cards_n) {
-    bool status = false;
-    int tries = 0;
+    //bool status = false;
+    //int tries = 0;
     /* Check if prime_settings is available
      * File doesn't exist or empty
      */
@@ -2003,6 +2003,7 @@ static bool enable_prime(const char *prime_settings,
         remove_prime_outputclass();
         remove_offload_serverlayout();
 
+#if 0
 unload_again:
         /* Unload the NVIDIA modules and enable pci power management */
         if (is_module_loaded("nvidia")) {
@@ -2024,6 +2025,8 @@ unload_again:
                 }
             }
         }
+#endif
+
         /* Set power control to "auto" to save power */
         enable_power_management(device);
     }
